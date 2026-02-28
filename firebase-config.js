67f5336c0c99aadd1fc9c38ce5c5e262aee8fbb4
idginity124 <yescamo125@protonmail.com>
2026-02-28 23:02:17 +0300

    Add Firebase admin panel

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,11 @@
 // ============================================================
 
 const FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyDPc5ZQg3Qqge-JXAC-_2QcuE6-vkIDIaQ",
-  authDomain:        "websites-9c326.firebaseapp.com",
-  projectId:         "websites-9c326",
-  storageBucket:     "websites-9c326.firebasestorage.app",
+  apiKey: "AIzaSyDPc5ZQg3Qqge-JXAC-_2QcuE6-vkIDIaQ",
+  authDomain: "websites-9c326.firebaseapp.com",
+  projectId: "websites-9c326",
+  storageBucket: "websites-9c326.firebasestorage.app",
   messagingSenderId: "616521643232",
-  appId:             "1:616521643232:web:8bbe38bc74593816938c85"
+  appId: "1:616521643232:web:8bbe38bc74593816938c85"
 };
-
 export default FIREBASE_CONFIG;
